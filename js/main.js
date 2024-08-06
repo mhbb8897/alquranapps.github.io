647a9d9d9a10b408089cf71ede7d8dc38b66f77e
mhbb8897 <mahbub.ahwani89@gmail.com>
2024-08-07 03:45:00 +0700

    fix functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("content").style.display = "flex";
+  document.getElementById("content").style.display = "none";
   document.getElementById("spinner-loading").style.display = "none";
 });
 
@@ -28,61 +28,68 @@ function loadData() {
       } else {
         filteredSurah.forEach((el) => {
           output += `<article class="surah_item">
-                            <h3>${el.number_of_surah}. ${el.name} ${el.name_translations.ar}</h3>
-                            <p>Arti Judul : ${el.name_translations.id}</p>
-                            <p>Tipe Surat : ${el.type}</p>
-                            <button class="read-translation" data-surah="${el.number_of_surah}">Baca</button>
-                            <button class="close-translation">Tutup</button>
-                            <div id="translation-${el.number_of_surah}" class="translation-wrapper"></div>
-                        </article>`;
+                        <h3>${el.number_of_surah}. ${el.name} ${el.name_translations.ar}</h3>
+                        <p>Arti Judul : ${el.name_translations.id}</p>
+                        <p>Tipe Surat : ${el.type}</p>
+                        <button class="read-translation" data-surah="${el.number_of_surah}">Baca</button>
+                        <div id="translation-${el.number_of_surah}" class="translation-wrapper"></div>
+                    </article>`;
         });
       }
 
       document.querySelector(".list-surah").innerHTML = output;
 
-      document.querySelectorAll(".read-translation").forEach((button) => {
+      // Button read and toggle (baca/tutup)
+      const readTranslation = document.querySelectorAll(".read-translation");
+
+      readTranslation.forEach((button) => {
         button.addEventListener("click", function () {
-          button.style.cursor = "auto";
           const numberSurah = button.getAttribute("data-surah");
           const translation = document.getElementById(
             `translation-${numberSurah}`
           );
-          button.disabled = true;
-          fetch(
-            `https://raw.githubusercontent.com/penggguna/QuranJSON/master/surah/${numberSurah}.json`
-          )
-            .then((response) => response.json())
-            .then((data) => {
-              data.verses.forEach((el) => {
-                const verseElement = document.createElement("p");
-                verseElement.innerText = `${el.number}. ${el.translation_id}`;
-                const surahtext = document.createElement("p");
-                surahtext.innerText = `${el.text}`;
 
-                translation.style.display = "block";
-                translation.appendChild(surahtext);
-                translation.appendChild(verseElement);
-              });
+          if (button.textContent === "Baca") {
+            // Disable all other buttons
+            readTranslation.forEach((btn) => {
+              btn.disabled = true;
+              btn.style.cursor = "auto";
             });
-        });
-      });
-      document.querySelectorAll(".close-translation").forEach((buttonClose) => {
-        buttonClose.addEventListener("click", function () {
-          const numberSurah =
-            buttonClose.previousElementSibling.getAttribute("data-surah");
-          const translation = document.getElementById(
-            `translation-${numberSurah}`
-          );
-          translation.style.display = "none";
-          const surahButton = document.querySelector(
-            `.read-translation[data-surah="${numberSurah}"]`
-          );
-          surahButton.disabled = false;
-          surahButton.style.cursor = "pointer";
+            button.disabled = false;
+            button.textContent = "Tutup";
+            button.style.cursor = "pointer";
+
+            fetch(
+              `https://raw.githubusercontent.com/penggguna/QuranJSON/master/surah/${numberSurah}.json`
+            )
+              .then((response) => response.json())
+              .then((data) => {
+                data.verses.forEach((el) => {
+                  const verseElement = document.createElement("p");
+                  verseElement.innerText = `${el.number}. ${el.translation_id}`;
+                  const surahtext = document.createElement("p");
+                  surahtext.innerText = `${el.text}`;
+
+                  translation.style.display = "block";
+                  translation.appendChild(surahtext);
+                  translation.appendChild(verseElement);
+                });
+              });
+          } else {
+            button.textContent = "Baca";
+            translation.style.display = "none";
+            translation.innerHTML = ""; 
+
+            // Enable all other buttons
+            readTranslation.forEach((btn) => {
+              btn.disabled = false;
+              btn.style.cursor = "pointer";
+            });
+          }
         });
       });
 
-      // spinner and show content
+      // Spinner and show content
       setTimeout(() => {
         document.getElementById("spinner-loading").style.display = "none";
         document.getElementById("content").style.display = "flex";
